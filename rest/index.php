bd6a47923b72db1e1e742c1297196e94ea4754f7
Dino Keco <dino.keco@gmail.com>
2022-03-21 10:08:39 +0100

    successfull install of flightphp

---
+++ rest/index.php
@@ -0,0 +1,13 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require '../vendor/autoload.php';
+
+Flight::route('/', function(){
+    echo 'hello world!';
+});
+
+Flight::start();
+?>
