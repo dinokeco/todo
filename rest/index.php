86c3db1a561b7933ac07f1da189d3e0112d04826
Dino Keco <dino.keco@gmail.com>
2022-03-21 10:17:26 +0100

    first restful endpoint

--- rest/index.php
+++ rest/index.php
@@ -3,10 +3,44 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require '../vendor/autoload.php';
+require_once 'dao/TodoDao.class.php';
+require_once '../vendor/autoload.php';
 
-Flight::route('/', function(){
-    echo 'hello world!';
+// CRUD operations for todos entity
+
+/**
+* List all todos
+*/
+Flight::route('/todos', function(){
+  $dao = new TodoDao();
+  $todos = $dao->get_all();
+  Flight::json($todos);
+});
+
+/**
+* List invidiual todo
+*/
+
+/**
+* add todo
+*/
+
+/**
+* update todo
+*/
+
+/**
+* delete todo
+*/
+
+
+
+Flight::route('/dorian', function(){
+    echo 'hello world DORIAN!';
+});
+
+Flight::route('/tin/@name', function($name){
+    echo 'hello world TIN!'. $name;
 });
 
 Flight::start();
