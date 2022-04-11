ba39025d02b9611138cbb32948e09d8f547dbe18
Dino Keco <dino.keco@gmail.com>
2022-04-11 11:50:47 +0200

    color picker

--- js/note-service.js
+++ js/note-service.js
@@ -16,7 +16,7 @@ var NoteService = {
         for(let i = 0; i < data.length; i++){
           html += `
           <div class="col-lg-3">
-            <div class="card">
+            <div class="card" style="background-color:`+data[i].color+`">
               <img class="card-img-top" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22286%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20286%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17ff3c8cf14%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17ff3c8cf14%22%3E%3Crect%20width%3D%22286%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22107.19140625%22%20y%3D%2296.3%22%3E286x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Card image cap">
               <div class="card-body">
                 <h5 class="card-title">`+ data[i].name +`</h5>
@@ -55,7 +55,7 @@ var NoteService = {
         dataType: "json",
         success: function(result) {
             $("#note-list").html('<div class="spinner-border" role="status"> <span class="sr-only"></span>  </div>');
-            ToDoService.list(); // perf optimization
+            NoteService.list(); // perf optimization
             $("#addNoteModal").modal("hide");
         }
       });
@@ -94,4 +94,8 @@ var NoteService = {
         }
       });
     },
+
+    choose_color: function(color){
+      $('#addNoteForm input[name="color"]').val(color);
+    }
 }
