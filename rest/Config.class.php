5d6ec4f7bdada5a298d2650b74f5f2635adabd72
Dino Keco <dino.keco@gmail.com>
2022-04-18 10:13:21 +0200

    make app configurable for hosting

---
+++ rest/Config.class.php
@@ -0,0 +1,26 @@
+<?php
+
+class Config {
+
+  public static function DB_HOST(){
+    return Config::get_env("DB_HOST", "localhost");
+  }
+  public static function DB_USERNAME(){
+    return Config::get_env("DB_USERNAME", "todo");
+  }
+  public static function DB_PASSWORD(){
+    return Config::get_env("DB_PASSWORD", "todo");
+  }
+  public static function DB_SCHEME(){
+    return Config::get_env("DB_SCHEME", "todo");
+  }
+  public static function DB_PORT(){
+    return Config::get_env("DB_PORT", "3306");
+  }
+
+  public static function get_env($name, $default){
+   return isset($_ENV[$name]) && trim($_ENV[$name]) != '' ? $_ENV[$name] : $default;
+  }
+}
+
+?>
--- rest/dao/BaseDao.class.php
+++ rest/dao/BaseDao.class.php
@@ -1,4 +1,6 @@
 <?php
+require_once __DIR__.'/../Config.class.php';
+
 class BaseDao{
 
   private $conn;
@@ -10,11 +12,11 @@ class BaseDao{
   */
   public function __construct($table_name){
     $this->table_name = $table_name;
-    $servername = "";
-    $username = "doadmin";
-    $password = "";
-    $schema = "todo";
-    $port = "25060";
+    $servername = Config::DB_HOST();
+    $username = Config::DB_USERNAME();
+    $password = Config::DB_PASSWORD();
+    $schema = Config::DB_SCHEME();
+    $port = Config::DB_PORT();
     $this->conn = new PDO("mysql:host=$servername;dbname=$schema;port=$port", $username, $password);
     // set the PDO error mode to exception
     $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
