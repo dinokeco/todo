ebe4bd199ff0500fffff4660078212309d4e7a9f
Dino Keco <dino.keco@gmail.com>
2022-05-09 11:01:47 +0200

    add middlware login

--- js/note-service.js
+++ js/note-service.js
@@ -10,28 +10,39 @@ var NoteService = {
     },
 
     list: function(){
-      $.get("rest/notes", function(data) {
-        $("#note-list").html("");
-        var html = "";
-        for(let i = 0; i < data.length; i++){
-          html += `
-          <div class="col-lg-3">
-            <div class="card" style="background-color:`+data[i].color+`">
-              <img class="card-img-top" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22286%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20286%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17ff3c8cf14%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17ff3c8cf14%22%3E%3Crect%20width%3D%22286%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22107.19140625%22%20y%3D%2296.3%22%3E286x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Card image cap">
-              <div class="card-body">
-                <h5 class="card-title">`+ data[i].name +`</h5>
-                <p class="card-text">`+ data[i].description +`</p>
-                <div class="btn-group" role="group">
-                  <button type="button" class="btn btn-primary note-button" onclick="NoteService.get(`+data[i].id+`)">Edit</button>
-                  <button type="button" class="btn btn-success note-button" onclick="TodoService.list_by_note_id(`+data[i].id+`)">Manage</button>
-                  <button type="button" class="btn btn-danger note-button" onclick="NoteService.delete(`+data[i].id+`)">Delete</button>
-                </div>
-              </div>
-            </div>
-          </div>
-          `;
-        }
-        $("#note-list").html(html);
+      $.ajax({
+         url: "rest/notes",
+         type: "GET",
+         beforeSend: function(xhr){
+           xhr.setRequestHeader('Authorization', localStorage.getItem('token'));
+         },
+         success: function(data) {
+           $("#note-list").html("");
+           var html = "";
+           for(let i = 0; i < data.length; i++){
+             html += `
+             <div class="col-lg-3">
+               <div class="card" style="background-color:`+data[i].color+`">
+                 <img class="card-img-top" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22286%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20286%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17ff3c8cf14%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17ff3c8cf14%22%3E%3Crect%20width%3D%22286%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22107.19140625%22%20y%3D%2296.3%22%3E286x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Card image cap">
+                 <div class="card-body">
+                   <h5 class="card-title">`+ data[i].name +`</h5>
+                   <p class="card-text">`+ data[i].description +`</p>
+                   <div class="btn-group" role="group">
+                     <button type="button" class="btn btn-primary note-button" onclick="NoteService.get(`+data[i].id+`)">Edit</button>
+                     <button type="button" class="btn btn-success note-button" onclick="TodoService.list_by_note_id(`+data[i].id+`)">Manage</button>
+                     <button type="button" class="btn btn-danger note-button" onclick="NoteService.delete(`+data[i].id+`)">Delete</button>
+                   </div>
+                 </div>
+               </div>
+             </div>
+             `;
+           }
+           $("#note-list").html(html);
+         },
+         error: function(XMLHttpRequest, textStatus, errorThrown) {
+           toastr.error(XMLHttpRequest.responseJSON.message);
+           UserService.logout();
+         }
       });
     },
 
--- rest/index.php
+++ rest/index.php
@@ -3,6 +3,9 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
 require_once __DIR__.'/../vendor/autoload.php';
 require_once __DIR__.'/services/NoteService.class.php';
 require_once __DIR__.'/services/TodoService.class.php';
@@ -15,6 +18,28 @@ Flight::register('noteService', 'NoteService');
 Flight::map('error', function(Exception $ex){
     // Handle error
     Flight::json(['message' => $ex->getMessage()], 500);
+});
+
+// middleware method for login
+Flight::route('/*', function(){
+  //perform JWT decode
+  $path = Flight::request()->url;
+  if ($path == '/login') return TRUE; // exclude login route from middleware
+
+  $headers = getallheaders();
+  if (@!$headers['Authorization']){
+    Flight::json(["message" => "Authorization is missing"], 403);
+    return FALSE;
+  }else{
+    try {
+      $decoded = (array)JWT::decode($headers['Authorization'], new Key(Config::JWT_SECRET(), 'HS256'));
+      Flight::set('user', $decoded);
+      return TRUE;
+    } catch (\Exception $e) {
+      Flight::json(["message" => "Authorization token is not valid"], 403);
+      return FALSE;
+    }
+  }
 });
 
 require_once __DIR__.'/routes/TodoRoutes.php';
