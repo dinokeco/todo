4ef14a571f670357c1cf36c07be7e87eeaa66bd0
Dino Keco <dino.keco@gmail.com>
2022-05-09 11:07:20 +0200

    auth header in other methods

--- js/todo-service.js
+++ js/todo-service.js
@@ -20,17 +20,28 @@ var TodoService = {
 
   list_by_note_id: function(note_id){
     $("#notes-todos").html('loading ...');
-    $.get("rest/notes/"+note_id+"/todos", function(data) {
-      var html = "";
-      for(let i = 0; i < data.length; i++){
-        html += `<div class="list-group-item note-todo-`+data[i].id+`">
-          <button class="btn btn-danger btn-sm float-end" onclick="TodoService.delete(`+data[i].id+`)">delete</button>
-          <p class="list-group-item-text">`+data[i].description+`</p>
-        </div>`;
-      }
-      $("#notes-todos").html(html);
-    });
 
+    $.ajax({
+       url: "rest/notes/"+note_id+"/todos",
+       type: "GET",
+       beforeSend: function(xhr){
+         xhr.setRequestHeader('Authorization', localStorage.getItem('token'));
+       },
+       success: function(data) {
+         var html = "";
+         for(let i = 0; i < data.length; i++){
+           html += `<div class="list-group-item note-todo-`+data[i].id+`">
+             <button class="btn btn-danger btn-sm float-end" onclick="TodoService.delete(`+data[i].id+`)">delete</button>
+             <p class="list-group-item-text">`+data[i].description+`</p>
+           </div>`;
+         }
+         $("#notes-todos").html(html);
+       },
+       error: function(XMLHttpRequest, textStatus, errorThrown) {
+         toastr.error(XMLHttpRequest.responseJSON.message);
+         UserService.logout();
+       }
+    });
     // note id populate and form validation
     $('#add-todo-form input[name="note_id"]').val(note_id);
     $('#add-todo-form input[name="created"]').val(TodoService.current_date());
@@ -53,6 +64,9 @@ var TodoService = {
     $.ajax({
       url: 'rest/todos/'+id,
       type: 'DELETE',
+      beforeSend: function(xhr){
+        xhr.setRequestHeader('Authorization', localStorage.getItem('token'));
+      },
       success: function(result) {
         toastr.success("Deleted !");
       },
